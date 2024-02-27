cd04a27b17d062959e641e2e61d0bd05c51a3c88
Wiktor <bramerwiktor@gmail.com>
2024-02-27 16:01:06 +0100

    modified:   game.py 	modified:   scripts/entities.py 	new file:   scripts/utils.py

--- game.py
+++ game.py
@@ -1,39 +1,44 @@
 import sys
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
 class Game:
     def __init__(self) -> None:
         pygame.init()
 
         WIDTH, HEIGHT = 640, 480
 
-        pygame.display.set_caption('CODE-33')
+        pygame.display.set_caption('Penguin Invasion')
 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         self.display = pygame.Surface((WIDTH/3, HEIGHT/3))
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('data/images/entities/player.png')
-        self.img.set_colorkey((0, 0, 0))
-
-        self.img_pos = [70, 70]
         self.movement = [False, False]
 
-        self.rect = pygame.Rect(100, 70, 16, 16)
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (17, 16))
+        self.rect = pygame.Rect(100, 50, 17, 16)
 
     def run(self):
         while True:
             self.display.fill((150,200,255))
 
             pygame.draw.rect(self.display, (255, 0, 0), self.rect)
-            self.img_pos[0] += (self.movement[1] - self.movement[0]) * 5
-            
-            
-            img_rect = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
 
-            if img_rect.colliderect(self.rect):
-                pygame.draw.rect(self.display, (0,100,255), self.rect)
+            if self.player.rect().colliderect(self.rect):
+                pygame.draw.rect(self.display, (0, 255, 0), self.rect)
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+
+            
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -44,13 +49,19 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-
-            self.display.blit(self.img, self.img_pos)
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
             
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,4 +1,26 @@
 import pygame
 
 class PhysicsEntity:
-    pass
+    def __init__(self, game, entity_type, pos, size) -> None:
+        self.game = game
+        self.type = entity_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = [0, 0]
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+
+    def rect(self):
+        # create a rect for entity
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+        
+    def update(self, movement=(0, 0)):
+        # frame_movement returns a tuple of x and y movement that is happening in one frame
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        # update the position of entity
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+    
+    def render(self, surf):
+        surf.blit(self.game.assets['player'], self.pos)
+        
